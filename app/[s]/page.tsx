bf789002d2312f1effa23ce1e177b22b44ff2ce7
happines <rhithesh1947@gmail.com>
2024-04-21 18:15:54 +0530

    fixed errors

---
+++ app/[s]/page.tsx
@@ -0,0 +1,58 @@
+"use client";
+import Image from "next/image";
+import Textarea from "@/components/Textarea";
+import Header from "@/components/Header";
+import React, { useState } from "react";
+import "react-quill/dist/quill.snow.css";
+import dynamic from "next/dynamic";
+import { useEffect, useRef } from "react";
+import io from "socket.io-client";
+
+const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
+const socket = io("http://localhost:8080", {
+	transports: ["websocket"],
+});
+
+export default function Home({ params }: { params: { slug: string } }) {
+	const [value, setValue] = useState("");
+	const isSocketConnected = useRef(false);
+
+	useEffect(() => {
+		if (!isSocketConnected.current) {
+			socket.connect();
+			socket.on("connect", () => {
+				console.log("connected");
+				isSocketConnected.current = true;
+			});
+
+			socket.emit("join-room", params.s);
+
+			socket.on("chat message", (data) => {
+				console.log("reciving from the other end", data);
+				setValue(data);
+			});
+		}
+		return () => {
+			socket.disconnect();
+			socket.off("connect");
+			socket.off("join-room");
+			socket.off("chat message");
+		};
+	}, []);
+
+	return (
+		<>
+			<Header />
+			<textarea
+				className=" border-2 border-black w-screen h-screen font-mono"
+				value={value}
+				onChange={(e) => {
+					setValue(e.target.value);
+					socket.emit("chat message", e.target.value, params.s);
+				}}
+			/>
+
+			<h1>{params.slug}</h1>
+		</>
+	);
+}
--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,40 @@ import Header from "@/components/Header";
 import React, { useState } from "react";
 import "react-quill/dist/quill.snow.css";
 import dynamic from "next/dynamic";
+import { useEffect } from "react";
+import io from "socket.io-client";
 
 const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
+const socket = io("http://localhost:8080", {
+	transports: ["websocket"],
+});
 
 export default function Home() {
 	const [value, setValue] = useState("");
+
+	useEffect(() => {
+		// Set up event listeners when component mounts
+		socket.connect();
+		socket.on("connect", () => {
+			console.log("connected");
+		});
+
+		socket.emit("join", "hithesh");
+
+		socket.on("message", (data) => {
+			console.log(data);
+		});
+
+		// Clean up event listeners when component unmounts
+		return () => {
+			socket.off("message");
+		};
+	}, []);
+
+	const sendMessage = () => {
+		socket.emit("message", value);
+		setValue(""); // Clear the input after sending the message
+	};
 
 	return (
 		<>
@@ -21,6 +50,7 @@ export default function Home() {
 				value={value}
 				onChange={(e) => {
 					console.log(e);
+					sendMessage();
 					setValue(e);
 				}}
 			/>
