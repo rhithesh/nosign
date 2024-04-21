982cb2cce7980eac891cb5810b3f4a6221e328b4
happines <rhithesh1947@gmail.com>
2024-04-21 22:31:44 +0530

    fixed errro

--- app/[s]/page.tsx
+++ app/[s]/page.tsx
@@ -9,7 +9,7 @@ import { useEffect, useRef } from "react";
 import io from "socket.io-client";
 
 const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
-const socket = io("https://nosignbackend.onrender.com:8080", {
+const socket = io("https://nosignbackend.onrender.com", {
 	transports: ["websocket"],
 });
 
